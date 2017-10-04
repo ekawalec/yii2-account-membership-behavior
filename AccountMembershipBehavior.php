a8812bca030166fc7b164689e79fdac27ef9479c
Edmund Kawalec <e.kawalec@s4studio.pl>
2017-10-05 00:14:34 +0200

    first uploads

---
+++ AccountMembershipBehavior.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jaszczomp
+ * Date: 2017.10.04
+ * Time: 23:35
+ */
+
+namespace backend\components\behaviors;
+
+use yii\behaviors\AttributeBehavior;
+use yii\db\BaseActiveRecord;
+
+
+/**
+ * AccountBehavior automatically fills the specified attributes with the current Account Id value.
+ *
+ * To use AccountBehavior, insert the following code to your ActiveRecord class:
+ *
+ * ```php
+ * use yii\behaviors\AccountBehavior;
+ *
+ * public function behaviors()
+ * {
+ *     return [
+ *         AccountBehavior::className(),
+ *     ];
+ * }
+ * ```
+ *
+ * By default, AccountBehavior will fill the `account_id` attributes with the current user account_id
+ * when the associated AR object is being inserted;
+ *
+ * Because attribute values will be set automatically by this behavior, they are usually not user input and should therefore
+ * not be validated, i.e. `account_id` should not appear in the [[\yii\base\Model::rules()|rules()]] method of the model.
+ *
+ * For the above implementation to work with MySQL database, please declare the columns(`account_id`) as int(11) for being Integer type.
+ *
+ * If your attribute names are different or you want to use a different way of calculating account_id,
+ * you may configure the [[accountIdAttribute]] and [[value]] properties like the following:
+ *
+ * ```php
+ * use yii\db\Expression;
+ *
+ * public function behaviors()
+ * {
+ *     return [
+ *         [
+ *             'class' => AccountBehavior::className(),
+ *             'accountIdAttribute' => 'instance_id',
+ *             'value' => Yii::$app->user->identity->instance_id,
+ *         ],
+ *     ];
+ * }
+ * ```
+ *
+ * @author Edmund Kawalec <e.kawalec@gmail.com>
+ * @since 2.0
+ */
+
+class AccountMembershipBehavior extends AttributeBehavior
+{
+
+    /**
+     * @var string the attribute that will receive timestamp value
+     * Set this property to false if you do not want to record the creation time.
+     */
+    public $accountIdAttribute = 'account_id';
+
+    /**
+     * @inheritdoc
+     *
+     * In case, when the value is `null`, the result of the actual account_id
+     * will be used as value.
+     */
+    public $value;
+
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        if (empty($this->attributes)) {
+            $this->attributes = [
+                BaseActiveRecord::EVENT_BEFORE_INSERT => $this->accountIdAttribute,
+                BaseActiveRecord::EVENT_BEFORE_VALIDATE => $this->accountIdAttribute,
+            ];
+        }
+    }
+
+    /**
+     * @inheritdoc
+     *
+     * In case, when the [[value]] is `null`, the result of the actual account_id
+     * will be used as value.
+     */
+    protected function getValue($event)
+    {
+        if ($this->value === null) {
+            return \Yii::$app->user->identity->{$this->accountIdAttribute};
+        }
+        return parent::getValue($event);
+    }
+
+
+}
