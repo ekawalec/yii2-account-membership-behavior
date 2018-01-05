2bb6a43ba0a87bd9b378819d6d68e3771df45089
Edmund Kawalec <e.kawalec@s4studio.pl>
2018-01-05 01:14:52 +0100

    move into behaviors folder

---
+++ behaviors/WeightBehavior.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * User: e.kawalec@s4studio.pl
+ * Date: 2017.10.04
+ * Time: 23:57
+ */
+
+namespace backend\behaviors;
+
+
+use yii\behaviors\AttributeBehavior;
+use yii\db\BaseActiveRecord;
+
+class WeightBehavior extends AttributeBehavior
+{
+
+    /**
+     * @var string the attribute that will receive value
+     */
+    public $weightAttribute = 'weight';
+
+    /**
+     * @var string the attribute that will count max +1 value
+     */
+    public $idAttribute = 'id';
+
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        if (empty($this->attributes)) {
+            $this->attributes = [
+                BaseActiveRecord::EVENT_BEFORE_INSERT => $this->weightAttribute,
+            ];
+        }
+    }
+
+    /**
+     * @inheritdoc
+     *
+     */
+    protected function getValue($event)
+    {
+        return ($this->owner)::find()->max($this->idAttribute)+1;
+    }
+
+
+
+}
